11d03a681fdb6b3224eb46cca13c00351661ecdc
Papp László <papp.lazslo.web@gmail.com>
2022-09-30 09:16:50 +0200

    time refactored

--- controller/UserController.php
+++ controller/UserController.php
@@ -40,11 +40,11 @@
 			$token = $_GET['token'];
 			$code = 'K8Ebl37ndddMK104S1E67885bcgbTfA82hUMC1a232d01XOb75gMkWKO1R7bIfjM17H5dFU9JeX2P5lG2bHPnchQ2Zb';
 			$hit = 5;
-			// token date time 
+			// token date current time 
 			$current_date = date("Y-m-d H:i:s", time());
-			$expire_time = strtotime("+ 6 days");
-			$expire_date = date("Y-m-d H:i:s", $expire_time);
-			$days=ceil(($expire_time-time())/60/60/24);
+			// token date time end
+			$expire_date = date("Y-m-d H:i:s", strtotime("+ 6 days"));
+			$days=ceil((strtotime("+ 22 days")-time())/60/60/24);
 			
 			
 			/* Token with hit count (5)
