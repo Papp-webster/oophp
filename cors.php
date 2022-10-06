402c57d4b10f696d49a19e9fc17cb952d2a563bc
Papp László <papp.lazslo.web@gmail.com>
2022-10-06 13:52:58 +0200

    cors fixed

--- cors.php
+++ cors.php
@@ -2,5 +2,5 @@
 
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
-header('Access-Control-Allow-Headers: X-Requested-With');
+header('Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With');
 header('Content-Type: application/json');
